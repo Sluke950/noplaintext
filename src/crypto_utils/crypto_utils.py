318c2a9825c0249cf447c44b60b35ed4d8e2335c
Luke Shuttleworth <sluke950@gmail.com>
2024-08-08 09:19:39 -0700

    Configured logging and error handling

--- src/crypto_utils/crypto_utils.py
+++ src/crypto_utils/crypto_utils.py
@@ -2,19 +2,49 @@ from cryptography.fernet import Fernet
 import os
 import logging
 
+LOGGER = logging.getLogger(__name__)
+
 def generate_key(file_path):
-    key = Fernet.generate_key()
-    with open(file_path, "wb") as key_file:
-        key_file.write(key)
+    try:
+        key = Fernet.generate_key()
+        with open(file_path, "wb") as key_file:
+            key_file.write(key)
+    except Exception:
+        LOGGER.exception(f"An error occured while generating key file '{file_path}'.")
+        raise
+    LOGGER.debug(f"Generated key file '{file_path}'.")
+
 
 def load_key(file_path: str):
-    with open(file_path, "rb") as key_file:
-        return key_file.read()
+    try:
+        with open(file_path, "rb") as key_file:
+            key = key_file.read()
+        
+    except Exception:
+        LOGGER.exception(f"An error occured while loading key file '{file_path}'.")
+        raise
+    LOGGER.debug(f"Loaded key file '{file_path}'.")
+    return key
+    
 
 def encrypt(message, key_file_path):
-    cipher_suite = Fernet(load_key(key_file_path))
-    return cipher_suite.encrypt(message.encode())
+    try:
+        cipher_suite = Fernet(load_key(key_file_path))
+        encrypted_message = cipher_suite.encrypt(message.encode())
+    except Exception:
+        LOGGER.exception('An error occured while decrypting message.')
+        raise
+    LOGGER.debug('Encrypted message successfully.')
+    return encrypted_message
 
 def decrypt(encrypted_message, key_file_path):
-    cipher_suite = Fernet(load_key(key_file_path))
-    return cipher_suite.decrypt(encrypted_message).decode()
+    try:
+        cipher_suite = Fernet(load_key(key_file_path))
+        decrypted_message = cipher_suite.decrypt(encrypted_message).decode()
+    except Exception:
+        LOGGER.exception('An error occured while decrypting message.')
+        raise
+    LOGGER.debug('Decrypted message successfully.')
+    return decrypted_message
+
+
